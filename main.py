7e5e23c6ab8eadcc00744988487acbda96b10dfe
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-03-27 20:31:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-num_char = len(input("What is your name? "))
-new_num_char = str(num_char)
-print("Your name has " + new_num_char + " characters.")
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+first_digit= two_digit_number[0]
+second_digit = two_digit_number[1]
+
+num = int(first_digit) + int(second_digit)
+print(num)
