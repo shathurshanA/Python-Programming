131af0f9dff5c3640df04671108d93280d831369
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-03-29 18:32:45 +0530

    Tip Calculator Project

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-age = input("What is your current age? ")
+print("Welcome to the tip calculator")
+total_bill = float(input("What was the total bill? $"))
+tip = float(input("What percentage tip would you like to give? 10,12 or 15 "))
+no_of_people = int(input("How many people to split the bill? "))
 
-age_int = int(age)
+#tip percentage = tip/100
+#bill with tip = tip percentage * total bill
 
-remaining_years = 90 - age_int
-days_remaining = remaining_years * 365
-weeks_remaining = remaining_years * 52
-months_remaining = remaining_years * 12
-
-print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
-
+bill_with_tip = tip / 100 * total_bill + total_bill
+bill_per_person = bill_with_tip / no_of_people
+total_bill_person= round(bill_per_person, 2)
+print(total_bill_person)
