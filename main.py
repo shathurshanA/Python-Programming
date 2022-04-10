2b93983d6d1d7c00081917824807d72c00b18a93
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-04-10 19:52:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-print("Welcome to the tip calculator")
-total_bill = float(input("What was the total bill? $"))
-tip = float(input("What percentage tip would you like to give? 10,12 or 15 "))
-no_of_people = int(input("How many people to split the bill? "))
+year = int(input("Which year do you want to check? "))
 
-#tip percentage = tip/100
-#bill with tip = tip percentage * total bill
-
-bill_with_tip = tip / 100 * total_bill + total_bill
-bill_per_person = bill_with_tip / no_of_people
-total_bill_person= round(bill_per_person, 2)
-print(total_bill_person)
+if year % 4 ==0:
+    if year % 100 == 0:
+        if year % 400 ==0:
+            print('Leap year')
+        else:
+            print('Not leap year')
+    else:
+        print('Leap Year')
+else:
+    print('Not leap year')
