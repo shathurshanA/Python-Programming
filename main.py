0a68b573cd4e0f5cea9346c04955e1cace523e43
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-04-16 08:59:38 +0530

    Add files via upload
    D3M35

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-year = int(input("Which year do you want to check? "))
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 
-if year % 4 ==0:
-    if year % 100 == 0:
-        if year % 400 ==0:
-            print('Leap year')
-        else:
-            print('Not leap year')
-    else:
-        print('Leap Year')
+bill = 0
+
+if size == "S":
+    bill +=15
+elif size == "M":
+    bill +=20
 else:
-    print('Not leap year')
+    bill+=25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill +=2
+    else:
+        bill +=3
+
+if extra_cheese == "Y":
+    bill +=1
+
+print(f"The Final Bill Is {bill}")
