068dfc21f18cc645f1b9df049bbd9fb448e8e22a
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-04-16 09:16:38 +0530

    D3M37

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
 
-bill = 0
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
 
-if size == "S":
-    bill +=15
-elif size == "M":
-    bill +=20
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+love_score = int(str(true) + str(love))
+
+if (love_score < 10) and (love_score > 90):
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your score is {love_score}, you are alright together.")
 else:
-    bill+=25
 
-if add_pepperoni == "Y":
-    if size == "S":
-        bill +=2
-    else:
-        bill +=3
-
-if extra_cheese == "Y":
-    bill +=1
-
-print(f"The Final Bill Is {bill}")
+    print(f"Your score in {love_score}")
