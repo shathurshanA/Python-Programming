895be9453984367b30e96100449edb593f7ddb8e
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-04-16 09:39:06 +0530

    Treasure Island

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
-print("Welcome to the Love Calculator!")
-name1 = input("What is your name? \n")
-name2 = input("What is their name? \n")
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
-combined_string = name1 + name2
-lower_case_string = combined_string.lower()
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-t = lower_case_string.count("t")
-r = lower_case_string.count("r")
-u = lower_case_string.count("u")
-e = lower_case_string.count("e")
+#Write your code below this line 👇
 
-true = t + r + u + e
+choice1 = input('You are at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 
-l = lower_case_string.count("l")
-o = lower_case_string.count("o")
-v = lower_case_string.count("v")
-e = lower_case_string.count("e")
-
-love = l + o + v + e
-
-love_score = int(str(true) + str(love))
-
-if (love_score < 10) and (love_score > 90):
-    print(f"Your score is {love_score}, you go together like coke and mentos.")
-elif (love_score >= 40) and (love_score <= 50):
-    print(f"Your score is {love_score}, you are alright together.")
+if choice1 == "left":
+    choice2 = input('You have come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    if choice2 ==  "wait":
+       choice3 = input( "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n ").lower()
+       if choice3 == "red":
+           print("It's a room full of fire. Game Over.")
+       elif choice3 == "yellow":
+           print("You found the treasure! You Win!")
+       elif choice3 == "blue":
+           print("You enter a room of beasts. Game Over.")
+       else:
+           print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You got attacked. Game Over")
 else:
-
-    print(f"Your score in {love_score}")
+    print("You fell into a hole. Game Over.")
