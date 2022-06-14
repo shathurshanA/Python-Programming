753f235547445d2193b382e29ed3b881c94cef84
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-06-14 18:49:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-total = 0
-for number in range(2, 101, 2):
-    total+=number
-print(total)
-
-total2= 0
 for number in range(1, 101):
-    if number % 2 == 0:
-        total2 +=number
+    if number % 3 == 0 and number % 5 == 0 :
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print("Number")
 
-print(total2)
+
