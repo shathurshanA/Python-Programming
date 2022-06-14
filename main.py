0fd1725e274d4c3d720b67aeba4f9e206b798717
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-06-14 18:38:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-student_heights = input("Input a list of student heights: ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+total = 0
+for number in range(2, 101, 2):
+    total+=number
+print(total)
 
-total_height = 0
-for height in student_heights:
-    total_height +=height
-print(total_height)
+total2= 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 +=number
 
-number_of_students = 0
-for students in student_heights:
-    number_of_students +=1
-print(number_of_students)
-
-
-average_height = round(total_height / number_of_students)
-print(average_height)
+print(total2)
