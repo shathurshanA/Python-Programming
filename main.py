f2c1cbe5286bfb77102a0df40040a324ef6771a2
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-06-19 17:12:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,21 @@
-#Password Generator Project
+#Step 1
+
+word_list = ["aardvark", "baboon", "camel"]
+
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+chosen_word = random.choice(word_list)
 
-#Eazy Level - Order not randomised:
+guess = input("Guess a letter: ").lower()
 
-password = ""
-for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
 
-for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-    print(password)
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-
-password_list = []
-
-for char in range(1, nr_letters + 1):
-    password_list.append(random.choice(letters))
-
-for char in range(1, nr_symbols + 1):
-    password_list.append(random.choice(symbols))
-
-for char in range(1, nr_numbers + 1):
-    password_list.append(random.choice(numbers))
-
-print(password_list)
-random.shuffle(password_list)
-print(password_list)
-
-for char in password list:
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
