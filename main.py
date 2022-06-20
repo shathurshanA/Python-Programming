8265970a466c7bf63ffb9a0890aec3ce7d7a9cd8
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-06-20 11:19:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62,
-}
-# 🚨 Don't change the code above 👆
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
 
-# TODO-1: Create an empty dictionary called student_grades.
-student_grades = {}
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
 
-for student in student_scores:
-    score = student_scores[student]
-    if score > 90:
-        student_grades[student] = "Outstanding"
-    elif score > 80:
-        student_grades[student] = "Exceeds Expectation"
-    elif score > 70:
-        student_grades[student] = "Acceptable"
-    else:
-        student_grades[student] = "Fail"
-
-
-# TODO-2: Write your code below to add the grades to student_grades.👇
+def add_new_country(country_visited, times_visited, cities_visited):
+  new_country = {}
+  new_country["country"] = country_visited
+  new_country["visits"] = times_visited
+  new_country["cities"] = cities_visited
+  travel_log.append(new_country)
 
 
-# 🚨 Don't change the code below 👇
-print(student_grades)
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+
