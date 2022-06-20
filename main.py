64151849d3ad543e5417c85a584016f55b1fb4a6
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-06-20 13:22:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
+#addition
 
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+def add(n1, n2):
+    return n1 + n2
 
+#sub
 
-def days_in_month(year, month):
+def subtract(n1, n2):
+    return n1 - n2
 
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year) and month == 2:
-        return 29
-    return month_days[month - 1]
+#multiply
 
+def multiply(n1, n2):
+    return n1 * n2
 
+#divide
 
-# 🚨 Do NOT change any of the code below
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
+def divide(n1, n2):
+    return n1 / n2
 
+operations = {
+    "+" :  add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+}
 
+num1 = int(input("What is the first number?: "))
 
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("wthat is the second number?: "))
+calculation_function = operation[operation_symbol]
+answer = calculation_function(num1 , num2)
 
-
-
-
-
-
-
-
-
-
+print(f"{num1} {operation_symbol} {num2} = {answer}")
