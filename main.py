63e2f153cac742b75cd522f1da0a77a22ffac7b1
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-06-30 10:47:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-year = int(input("Which year do you want to check?"))
-
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap year.")
-        else:
-            print("Not leap year.")
-    else:
-        print("Leap year.")
-else:
-    print("Not leap year.")
+for number in range(1, 101):
+  print(f"Currently on number {number}")
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
