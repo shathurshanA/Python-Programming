9590c77b815cceac63dff5df04ae55c71339c160
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-06-30 10:33:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,6 @@
-#addition
+number = int(input("Which number do you want to check?"))
 
-def add(n1, n2):
-    return n1 + n2
-
-#sub
-
-def subtract(n1, n2):
-    return n1 - n2
-
-#multiply
-
-def multiply(n1, n2):
-    return n1 * n2
-
-#divide
-
-def divide(n1, n2):
-    return n1 / n2
-
-operations = {
-    "+" :  add,
-    "-" : subtract,
-    "*" : multiply,
-    "/" : divide
-}
-
-num1 = int(input("What is the first number?: "))
-
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("wthat is the second number?: "))
-calculation_function = operation[operation_symbol]
-answer = calculation_function(num1 , num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
