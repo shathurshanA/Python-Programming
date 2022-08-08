dd584161eb324c18be652febc8ea78bc1e8a5dfb
shathurshanA <101967876+shathurshanA@users.noreply.github.com>
2022-08-08 19:21:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-import colorgram
+from turtle import Turtle, Screen
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+tim = Turtle()
+screen = Screen()
 
-print(rgb_colors)
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+clear()
+
+screen.exitonclick()
+
